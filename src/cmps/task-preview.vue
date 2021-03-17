1396c4da39e00dcb84feafddd44269a7b8d5a194
Roi Levy <levy6161@gmail.com>
2021-03-17 19:39:22 +0200

    added task members status

--- src/cmps/task-preview.vue
+++ src/cmps/task-preview.vue
@@ -1,11 +1,35 @@
 <template>
-    <h4> {{task.title}} </h4>
+  <div class="task flex">
+    <h4>{{ task.title }}</h4>
+    <img
+      v-for="member in task.members"
+      :key="member._id"
+      :src="member.imgUrl"
+      style="width: 30px"
+    />
+    <h3 @click="isSelectingStatus=!isSelectingStatus">{{task.statusId}}</h3>
+    <status-picker v-if="isSelectingStatus"></status-picker>
+    <input type="date" name="due-date" id="due-date" v-model="dueDate" />
+  </div>
 </template>
 
 <script>
+import moment from "moment";
+import statusPicker from './status-picker.vue'
 export default {
-props: ['task'],
-
-}
+  props: ["task"],
+  data() {
+    return {
+      dueDate: "",
+      isSelectingStatus:false
+    };
+  },
+  created() {
+    console.log("task", this.task);
+  },
+  components:{
+      statusPicker
+  }
+};
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import './assets/styles/styles.scss'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 
 import { library } from '@fortawesome/fontawesome-svg-core'

ac94f4f45ef9d8b673f077b453f258795add7f8c
dorrang <summ.pie@gmail.com>
2021-03-17 15:13:46 +0200

    group

--- src/cmps/group.vue
+++ src/cmps/group.vue
@@ -1,13 +1,51 @@
 <template>
-  
+  <section class="group-container">
+    <h2 class="group-title">{{ group.title }}</h2>
+    <task-preview v-for="task in group.tasks" :key="task.id" :task="task" />
+    <form @submit.prevent="addTask()">
+      <input
+        class="add-task-input"
+        type="text"
+        placeholder="+Add Task"
+        v-model="taskToEdit.title"
+      />
+      <button v-if="taskToEdit" class="add-task">Add</button>
+    </form>
+  </section>
 </template>
 
 <script>
-export default {
+import taskPreview from "./task-preview.vue";
 
-}
+export default {
+  name: 'group',
+  props: ['group'],
+  data() {
+    return {
+      taskToEdit: {
+        title: '',
+      },
+    };
+  },
+  methods: {
+    async addTask() {
+      await this.$store.dispatch({ type: 'addTask', task: this.taskToEdit });
+      this.taskToEdit = { title: '' };
+    },
+  },
+  computed: {
+    loggedInUser() {
+      return this.$store.getters.loggedinUser;
+    },
+  },
+  created() {
+    this.$store.dispatch({ type: 'loadGroup' });
+  },
+  components: {
+    taskPreview,
+  },
+};
 </script>
 
 <style>
-
 </style>

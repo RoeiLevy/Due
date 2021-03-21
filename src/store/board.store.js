a1cf5c2f67db624e0fadd78e5f45c128b88d0aef
Yonatan Mizrahi <yonatanmiz963@gmail.com>
2021-03-21 16:09:38 +0200

    moving all emits to board component

--- src/store/board.store.js
+++ src/store/board.store.js
@@ -1,5 +1,5 @@
 import { boardService } from '../services/board.service'
-import { utilService } from '../services/util.service';
+// import { utilService } from '../services/util.service';
 
 export const boardStore = {
     state: {
@@ -34,39 +34,39 @@ export const boardStore = {
         setBoards(state, { boards }) {
             state.boards = boards;
         },
-        addNewGroup(state) {
-            state.currBoard.groups.push(boardService.getEmptyGroup());
-        },
-        saveGroup(state, { group }) {
-            const idx = state.currBoard.groups.findIndex(g => g.id === group.id);
-            state.currBoard.groups.splice(idx, 1, group);
-        },
-        removeGroup(state, { groupId }) {
-            const idx = state.currBoard.groups.findIndex(g => g.id === groupId);
-            state.currBoard.groups.splice(idx, 1);
-        },
-        addTask(state, { task, groupId }) {
-            const currGroupIdx = state.currBoard.groups.findIndex(group => group.id === groupId);
-            var newGroup = { ...state.currBoard.groups[currGroupIdx] }
-            newGroup.tasks.push(task)
-            state.currBoard.groups.splice(currGroupIdx, 1, newGroup);
-            // if (!currGroup || !currGroup.tasks) {
-            //     currGroup.tasks = [];
-            // }
-            // currGroup.tasks.push(task);
-        },
-        updateTask(state, { task, groupId }) {
-            // console.log('update task - task', task, ' group id', groupId)
-            const groupIdx = state.currBoard.groups.findIndex(group => group.id === groupId);
-            const taskIdx = state.currBoard.groups[groupIdx].tasks.findIndex(item => item.id === task.id);
-            state.currBoard.groups[groupIdx].tasks.splice(taskIdx, 1, task);
-        },
-        removeTask(state, { taskId, groupId }) {
-            // console.log('removing task - task', taskId, ' group id', groupId)
-            const groupIdx = state.currBoard.groups.findIndex(group => group.id === groupId);
-            const taskIdx = state.currBoard.groups[groupIdx].tasks.findIndex(item => item.id === taskId);
-            state.currBoard.groups[groupIdx].tasks.splice(taskIdx, 1);
-        },
+        // addNewGroup(state) {
+        //     state.currBoard.groups.push(boardService.getEmptyGroup());
+        // },
+        // saveGroup(state, { group }) {
+        //     const idx = state.currBoard.groups.findIndex(g => g.id === group.id);
+        //     state.currBoard.groups.splice(idx, 1, group);
+        // },
+        // removeGroup(state, { groupId }) {
+        //     const idx = state.currBoard.groups.findIndex(g => g.id === groupId);
+        //     state.currBoard.groups.splice(idx, 1);
+        // },
+        // addTask(state, { task, groupId }) {
+        //     const currGroupIdx = state.currBoard.groups.findIndex(group => group.id === groupId);
+        //     var newGroup = { ...state.currBoard.groups[currGroupIdx] }
+        //     newGroup.tasks.push(task)
+        //     state.currBoard.groups.splice(currGroupIdx, 1, newGroup);
+        //     // if (!currGroup || !currGroup.tasks) {
+        //     //     currGroup.tasks = [];
+        //     // }
+        //     // currGroup.tasks.push(task);
+        // },
+        // updateTask(state, { task, groupId }) {
+        //     // console.log('update task - task', task, ' group id', groupId)
+        //     const groupIdx = state.currBoard.groups.findIndex(group => group.id === groupId);
+        //     const taskIdx = state.currBoard.groups[groupIdx].tasks.findIndex(item => item.id === task.id);
+        //     state.currBoard.groups[groupIdx].tasks.splice(taskIdx, 1, task);
+        // },
+        // removeTask(state, { taskId, groupId }) {
+        //     // console.log('removing task - task', taskId, ' group id', groupId)
+        //     const groupIdx = state.currBoard.groups.findIndex(group => group.id === groupId);
+        //     const taskIdx = state.currBoard.groups[groupIdx].tasks.findIndex(item => item.id === taskId);
+        //     state.currBoard.groups[groupIdx].tasks.splice(taskIdx, 1);
+        // },
     },
     actions: {
         async loadBoard(context, { boardId }) {
@@ -86,85 +86,87 @@ export const boardStore = {
         async loadBoards(context) {
             try {
                 const boards = await boardService.query();
-                console.log('boards:', boards)
                 context.commit({ type: 'setBoards', boards });
+                return boards
             } catch (err) {
                 console.log('boardStore: Error in loadBoards', err);
                 throw err;
             }
         },
-        async saveBoard(context, board) {
+        async saveBoard(context, { boardToSave } ) {
             try {
-                const savedBoard = await boardService.update(board);
-                console.log('board:', savedBoard);
-                context.commit({ type: 'setBoard', board });
+                const savedBoard = await boardService.update(boardToSave);
+                context.commit({ type: 'setBoard', board:  JSON.parse(JSON.stringify(boardToSave)) });
+                console.log('board in store:', savedBoard);
+                return savedBoard
             } catch (err) {
                 console.log('boardStore: Error in loadBoards', err);
                 throw err;
-            }
-        },
-        async saveGroup(context, group) {
-            try {
-                const savedGroup = await boardService.saveGroup(group, context.state.currBoard._id);
-                context.commit({ type: 'saveGroup', group: savedGroup })
-                // return savedGroup;
-            } catch (err) {
-                console.log('Board store:Error in saveGroup', err);
-                throw err;
-            }
-        },
-        async removeGroup({ commit, state }, { groupId }) {
-            try {
-                await boardService.removeGroup(groupId, state.currBoard._id,)
-                commit({ type: 'removeGroup', groupId })
-                return groupId;
-            } catch (err) {
-                console.log('err:', err)
             }
         },
         async addNewBoard(context) {
             try {
                 var newBoard = boardService.getEmptyBoard();
-                return await boardService.add(newBoard);
+                const addedBoard = await boardService.add(newBoard);
+                return addedBoard
             } catch (err) {
                 console.log('boardStore: Error in Adding New Board', err)
                 throw err
             }
         },
-        async updateTask(context, payload) {
-            try {
-                const task = await boardService.updateTask(payload.taskToEdit, payload.groupId, context.state.currBoard._id)
-                context.commit({ type: 'updateTask', task, groupId: payload.groupId })
-                return task;
-            } catch (err) {
-                console.log('boardStore: Error in updateTask', err)
-                throw err
-            }
-        },
-        async removeTask(context, payload) {
-            try {
-                console.log(payload.taskId, payload.groupId);
-                const removed = await boardService.removeTask(payload.taskId, payload.groupId, context.state.currBoard._id)
-                context.commit({ type: 'removeTask', taskId: payload.taskId, groupId: payload.groupId })
-                return removed;
-            } catch (err) {
-                console.log('boardStore: Error in updateTask', err)
-                throw err
-            }
-        },
-        async addTask(context, payload) {
-            try {
-                payload.taskToEdit.id = utilService.makeId();
-                const task = await boardService.addTask(payload.taskToEdit, payload.groupId, context.state.currBoard._id)
-                await context.dispatch({ type: 'loadBoard', boardId: context.state.currBoard._id })
-                return task;
-            } catch (err) {
-                console.log('boardStore: Error in addTask', err)
-                throw err
-            }
-        },
-        async getTask(context, { taskId }) {
-            console.log(taskId);
-        }
+        // async saveGroup(context, group) {
+        //     try {
+        //         const savedGroup = await boardService.saveGroup(group, context.state.currBoard._id);
+        //         context.commit({ type: 'saveGroup', group: savedGroup })
+        //         // return savedGroup;
+        //     } catch (err) {
+        //         console.log('Board store:Error in saveGroup', err);
+        //         throw err;
+        //     }
+        // },
+        // async removeGroup({ commit, state }, { groupId }) {
+        //     try {
+        //         await boardService.removeGroup(groupId, state.currBoard._id,)
+        //         commit({ type: 'removeGroup', groupId })
+        //         return groupId;
+        //     } catch (err) {
+        //         console.log('err:', err)
+        //     }
+        // },
+        // async updateTask(context, payload) {
+        //     try {
+        //         const task = await boardService.updateTask(payload.taskToEdit, payload.groupId, context.state.currBoard._id)
+        //         context.commit({ type: 'updateTask', task, groupId: payload.groupId })
+        //         return task;
+        //     } catch (err) {
+        //         console.log('boardStore: Error in updateTask', err)
+        //         throw err
+        //     }
+        // },
+        // async removeTask(context, payload) {
+        //     try {
+        //         console.log(payload.taskId, payload.groupId);
+        //         const removed = await boardService.removeTask(payload.taskId, payload.groupId, context.state.currBoard._id)
+        //         context.commit({ type: 'removeTask', taskId: payload.taskId, groupId: payload.groupId })
+        //         return removed;
+        //     } catch (err) {
+        //         console.log('boardStore: Error in updateTask', err)
+        //         throw err
+        //     }
+        // },
+        // async addTask(context, payload) {
+        //     try {
+        //         payload.taskToEdit.id = utilService.makeId();
+        //         const task = await boardService.addTask(payload.taskToEdit, payload.groupId, context.state.currBoard._id)
+        //         await context.dispatch({ type: 'loadBoard', boardId: context.state.currBoard._id })
+        //         return task;
+        //     } catch (err) {
+        //         console.log('boardStore: Error in addTask', err)
+        //         throw err
+        //     }
+        // },
+        // async getTask(context, { taskId }) {
+        //     console.log(taskId);
+        // }
     }
 }

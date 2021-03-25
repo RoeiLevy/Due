51a744d3035bf21deb16e8c388ced41a72521b13
Yonatan Mizrahi <yonatanmiz963@gmail.com>
2021-03-25 15:02:24 +0200

    gm

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,33 +3,45 @@
     <header class="home-header">
       <router-link to="/">
         <h2 class="due">Due<span class="com">.com</span></h2>
-        <!-- <img class="logo" src="@/assets/imgs/logo.png" /> -->
       </router-link>
-      <div v-if="!loggedInUser || loggedInUser.fullname === 'Guest'" class="nav-links">
+      <div
+        v-if="!loggedInUser || loggedInUser.fullname === 'Guest'"
+        class="nav-links"
+      >
         <router-link to="/login">Login</router-link>
         <span>|</span>
         <router-link to="/signup">Sign Up</router-link>
       </div>
       <div v-else class="nav-links">
-      <h2 class="username" v-if="loggedInUser">Hello {{ loggedInUser.fullname }}</h2>
+        <h2 class="username" v-if="loggedInUser">
+          Hello {{ loggedInUser.fullname }}
+        </h2>
         <span>|</span>
         <button @click="logout" class="logout-btn">Logout</button>
       </div>
     </header>
-    <main class="main-content">
+    <main class="main-content flex column">
+      <section class="info-container flex column">
+        <!-- <h1>DUE</h1> -->
+      <div class="logo-container">
 
-    <section class="info-container flex column">
-     
-      <h1>DUE</h1>
-      <h2>
-        Planning, tracking and delivering your team’s best work has never been
-        easier.
-      </h2>
-      <h4>
-        Manage and control your team projects. A platform for work management,
-        including tracking projects, deadlines, and team collaboration.
-      </h4>
-    </section>
+        <img class="logo" src="@/assets/imgs/logo.png" />
+      </div>
+        <div class="desc">
+          <h2>
+            Planning, tracking and delivering your team’s best work has never
+            been easier.
+          </h2>
+          <h4>
+            Manage and control your team projects. A platform for work
+            management, including tracking projects, deadlines, and team
+            collaboration.
+          </h4>
+        </div>
+      </section>
+      <el-button @click="pushRoute" class="get-started" type="success" round
+        >Get Started</el-button
+      >
     </main>
   </div>
 </template>

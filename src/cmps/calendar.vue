4a32c674d74b0c2c4dd6d8d53ed3f9e085511a0a
Roi Levy <levy6161@gmail.com>
2021-03-28 11:18:02 +0300

    calendar

--- src/cmps/calendar.vue
+++ src/cmps/calendar.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-      <v-calendar
+  <div>
+    <v-calendar
       class="custom-calendar max-w-full"
       :masks="masks"
       :attributes="attributes"
@@ -15,7 +15,6 @@
               v-for="attr in attributes"
               :key="attr.key"
               class="text-xs leading-tight rounded-sm p-1 mt-0 mb-1"
-              :class="attr.customData.class"
             >
               {{ attr.customData.title }}
             </p>
@@ -23,99 +22,120 @@
         </div>
       </template>
     </v-calendar>
-</div>
+  </div>
 </template>
 
 
 <script>
-import vCalendar from 'v-calendar/lib/components/calendar.umd'
+import vCalendar from "v-calendar/lib/components/calendar.umd";
 export default {
+  props: ["board"],
   data() {
     const month = new Date().getMonth();
     const year = new Date().getFullYear();
     return {
       masks: {
-        weekdays: 'WWW',
+        weekdays: "WWW",
       },
-      attributes: [
-        {
-          key: 1,
-          customData: {
-            title: 'Lunch with mom.',
-            class: 'bg-red-600 text-white',
-          },
-          dates: new Date(year, month, 1),
-        },
-        {
-          key: 2,
-          customData: {
-            title: 'Take Noah to basketball practice',
-            class: 'bg-blue-500 text-white',
-          },
-          dates: new Date(year, month, 2),
-        },
-        {
-          key: 3,
-          customData: {
-            title: "Noah's basketball game.",
-            class: 'bg-blue-500 text-white',
-          },
-          dates: new Date(year, month, 5),
-        },
-        {
-          key: 4,
-          customData: {
-            title: 'Take car to the shop',
-            class: 'bg-indigo-500 text-white',
-          },
-          dates: new Date(year, month, 5),
-        },
-        {
-          key: 4,
-          customData: {
-            title: 'Meeting with new client.',
-            class: 'bg-teal-500 text-white',
-          },
-          dates: new Date(year, month, 7),
-        },
-        {
-          key: 5,
-          customData: {
-            title: "Mia's gymnastics practice.",
-            class: 'bg-pink-500 text-white',
-          },
-          dates: new Date(year, month, 11),
-        },
-        {
-          key: 6,
-          customData: {
-            title: 'Cookout with friends.',
-            class: 'bg-orange-500 text-white',
-          },
-          dates: { months: 5, ordinalWeekdays: { 2: 1 } },
-        },
-        {
-          key: 7,
-          customData: {
-            title: "Mia's gymnastics recital.",
-            class: 'bg-pink-500 text-white',
-          },
-          dates: new Date(year, month, 22),
-        },
-        {
-          key: 8,
-          customData: {
-            title: 'Visit great grandma.',
-            class: 'bg-red-600 text-white',
-          },
-          dates: new Date(year, month, 25),
-        },
-      ],
+      //   attributes: [
+      //     {
+      //       key: 1,
+      //       customData: {
+      //         title: "Lunch with mom.",
+      //         class: "bg-red-600 text-white",
+      //       },
+      //       dates: new Date(year, month, 1),
+      //     },
+      //     {
+      //       key: 2,
+      //       customData: {
+      //         title: "Take Noah to basketball practice",
+      //         class: "bg-blue-500 text-white",
+      //       },
+      //       dates: new Date(year, month, 2),
+      //     },
+      //     {
+      //       key: 3,
+      //       customData: {
+      //         title: "Noah's basketball game.",
+      //         class: "bg-blue-500 text-white",
+      //       },
+      //       dates: new Date(year, month, 5),
+      //     },
+      //     {
+      //       key: 4,
+      //       customData: {
+      //         title: "Take car to the shop",
+      //         class: "bg-indigo-500 text-white",
+      //       },
+      //       dates: new Date(year, month, 5),
+      //     },
+      //     {
+      //       key: 4,
+      //       customData: {
+      //         title: "Meeting with new client.",
+      //         class: "bg-teal-500 text-white",
+      //       },
+      //       dates: new Date(year, month, 7),
+      //     },
+      //     {
+      //       key: 5,
+      //       customData: {
+      //         title: "Mia's gymnastics practice.",
+      //         class: "bg-pink-500 text-white",
+      //       },
+      //       dates: new Date(year, month, 11),
+      //     },
+      //     {
+      //       key: 6,
+      //       customData: {
+      //         title: "Cookout with friends.",
+      //         class: "bg-orange-500 text-white",
+      //       },
+      //       dates: { months: 5, ordinalWeekdays: { 2: 1 } },
+      //     },
+      //     {
+      //       key: 7,
+      //       customData: {
+      //         title: "Mia's gymnastics recital.",
+      //         class: "bg-pink-500 text-white",
+      //       },
+      //       dates: new Date(year, month, 22),
+      //     },
+      //     {
+      //       key: 8,
+      //       customData: {
+      //         title: "Visit great grandma.",
+      //         class: "bg-red-600 text-white",
+      //       },
+      //       dates: new Date(year, month, 25),
+      //     },
+      //   ],
     };
   },
-  components:{
-    vCalendar
-  }
+  computed: {
+    attributes() {
+      const map = [];
+      var idx = 0;
+      this.board.groups.forEach((group) => {
+        group.tasks.forEach((task) => {
+          if (task.dueDate)
+            map.push({
+              key: idx++,
+              customData: {
+                title: task.title,
+              },
+              dates: new Date(task.dueDate),
+            });
+        });
+      });
+      console.log("map:", map);
+      return map;
+    },
+  },
+  components: {
+    vCalendar,
+  },
 };
 </script>
 <style lang="postcss" scoped>
